b062cc1a5f1792432fc8ffe40c7bec4d192a4a64
Enes Demirag <enesdemirag1@hotmail.com>
2018-07-26 17:51:05 +0300

    Build filters package

---
+++ build/filters/CMakeFiles/volex_filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/volex_filter.dir/src/volex_filter.cpp.o"
+  "volex_filter.pdb"
+  "volex_filter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/volex_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
