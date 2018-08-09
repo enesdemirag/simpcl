bcb7da68e96c20d00ec3122ad75d8f42ef8d4de0
Enes Demirağ <demirag16@itu.edu.tr>
2018-08-09 12:07:46 +0300

    Change filter functions

--- src/filters/src/region_growing_segmentation.cpp
+++ src/filters/src/region_growing_segmentation.cpp
@@ -1,5 +1,6 @@
 // Color-based Region Growing Segmentation for removing the ground
 // http://pointclouds.org/documentation/tutorials/region_growing_rgb_segmentation.php
+// TODO: Fix "Assertion `px != 0' failed. Aborted (core dumped)" error
 
 // Dependencies
 #include <iostream>
@@ -13,11 +14,11 @@
 #include <pcl/search/search.h>
 #include <pcl/search/kdtree.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl/filters/passthrough.h>
+#include <pcl/features/normal_3d.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl/segmentation/region_growing_rgb.h>
+#include <pcl/segmentation/region_growing.h>
 
 // Definitions
 ros::Publisher pub;
@@ -30,30 +31,32 @@ void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     pcl::search::Search <pcl::PointXYZRGB>::Ptr tree = boost::shared_ptr<pcl::search::Search<pcl::PointXYZRGB> > (new pcl::search::KdTree<pcl::PointXYZRGB>);
     pcl::PointCloud <pcl::PointXYZRGB>::Ptr raw_cloud (new pcl::PointCloud <pcl::PointXYZRGB>);
     pcl::IndicesPtr indices(new std::vector <int>);
+    pcl::PointCloud <pcl::Normal>::Ptr normals (new pcl::PointCloud <pcl::Normal>);
 
-    pcl::PassThrough<pcl::PointXYZRGB> pass;
-    pass.setInputCloud(raw_cloud);
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits(-1.0, 1.0);
-    pass.filter(*indices);
+    // Generate Normals
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimator;
+    normal_estimator.setSearchMethod(tree);
+    normal_estimator.setInputCloud(raw_cloud);
+    normal_estimator.setKSearch(50);
+    normal_estimator.compute(*normals);
 
-    pcl::RegionGrowingRGB<pcl::PointXYZRGB> reg;
-    reg.setInputCloud(raw_cloud);
-    reg.setIndices(indices);
+    // Perform Region Growing Segmentation
+    pcl::RegionGrowing<pcl::PointXYZRGB, pcl::Normal> reg;
+    reg.setMinClusterSize(50);
+    reg.setMaxClusterSize(1000000);
     reg.setSearchMethod(tree);
-    reg.setDistanceThreshold(10);
-    reg.setPointColorThreshold(6);
-    reg.setRegionColorThreshold(5);
-    reg.setMinClusterSize(600);
+    reg.setNumberOfNeighbours(30);
+    reg.setInputCloud(raw_cloud);
+    reg.setInputNormals(normals);
+    reg.setSmoothnessThreshold(3.0 / 180.0 * M_PI);
+    reg.setCurvatureThreshold(1.0);
 
-    std::vector <pcl::PointIndices> clusters;
-    reg.extract(clusters);
+    pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud (new pcl::PointCloud <pcl::PointXYZRGB>);
+    colored_cloud = reg.getColoredCloud();
 
-    pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud();
-
-    sensor_msgs::PointCloud2 cloud_out;
-    pcl::toROSMsg(*colored_cloud, cloud_out);
-    pub.publish(cloud_out);
+    /*sensor_msgs::PointCloud2 cloud_out;
+    pcl::toROSMsg(*colored_cloud, cloud_out);*/
+    pub.publish(colored_cloud);
 }
 
 // main
@@ -72,7 +75,7 @@ int main(int argc, char **argv)
     ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>(subscribed_topic, 1, cloud_cb);
 
     // Create Publisher
-    pub = n.advertise<sensor_msgs::PointCloud2>(published_topic, 1);
+    pub = n.advertise<pcl::PointCloud<pcl::PointXYZRGB> >(published_topic, 1); //<sensor_msgs::PointCloud2>
 
     // Spin
     ros::spin();
