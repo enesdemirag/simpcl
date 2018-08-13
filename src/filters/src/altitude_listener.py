7e7c5f643e36d555b1ced532157b5913628e6d59
enesdemirag <enesdemirag1@hotmail.com>
2018-08-13 14:29:11 +0300

    Create checking loop

--- src/filters/src/altitude_listener.py
+++ src/filters/src/altitude_listener.py
@@ -3,23 +3,26 @@
 import rospy
 
 altitude0, altitude1 = 0
-threshold = 0.3 # 0.3 meter
+
 def callback(msg):
     altitude1 = altitude0
     altitude0 = msg
 
 def main():
+    threshold = float(rospy.get_param("~threshold")
     rospy.init_node('altitude_listener', anonymous=True)
     r = rospy.Rate(20)
     rospy.Subscriber("/laserscan", Float32, callback, 1)
     pub = rospy.Publisher("/altitude_tracker", queue_size=10)
     while(True):
-        if((altitude1 - altitude0) < threshold):
-            pub.publish(altitude1) # There is no obstacle between drone and ground
+        if(abs(altitude1 - altitude0) < threshold)):
+            pub.publish(altitude1); # There is no obstacle between drone and ground
         else:
-            pub.publish(0) # Obstacle found. Do not remove points under obstacle
-        r.sleep()
-        rospy.spin()
+            while(True):
+                if(abs(altitude1 - altitude0 < threshold):
+                    pub.publish(0); # Obstacle found. Do not remove points under obstacle
+                else:
+                    break
 
 if __name__ == '__main__':
     main()
