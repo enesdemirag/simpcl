5580d61a110d2d1bf51179315dc994e7b39a161d
Enes Demirag <enesdemirag1@hotmail.com>
2018-08-20 17:26:38 +0300

    Add transformation and rotation functions

--- src/filters/src/pointmatcher.cpp
+++ src/filters/src/pointmatcher.cpp
@@ -10,6 +10,7 @@
 #include "pointmatcher/PointMatcher.h"
 #include "pointmatcher_ros/point_cloud.h"
 #include "pointmatcher_ros/transform.h"
+#include "sensor_msgs/PointCloud2.h"
 
 using namespace std;
 using namespace PointMatcherSupport;
@@ -22,12 +23,84 @@ typedef PointMatcher<float> PM;
 typedef PM::DataPoints DP;
 DP cloud1;
 DP cloud2;
+bool isVerbose = false;
+string initTranslation("0,0,0");
+string initRotation("1,0,0;0,1,0;0,0,1");
 
 // callback
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
     cloud2 = cloud1;
-    cloud1 = PointMatcher_ros::rosMsgToPointMatcherCloud(*cloud_msg);
+    cloud1= rosMsgToPointMatcherCloud(*cloud_msg);
+}
+
+PM::TransformationParameters parseTranslation(string& translation, const int cloudDimension)
+{
+	PM::TransformationParameters parsedTranslation;
+	parsedTranslation = PM::TransformationParameters::Identity(cloudDimension+1,cloudDimension+1);
+	translation.erase(std::remove(translation.begin(), translation.end(), '['), translation.end());
+	translation.erase(std::remove(translation.begin(), translation.end(), ']'), translation.end());
+	std::replace( translation.begin(), translation.end(), ',', ' ');
+	std::replace( translation.begin(), translation.end(), ';', ' ');
+
+	float translationValues[3] = {0};
+	stringstream translationStringStream(translation);
+	for(int i = 0; i < cloudDimension; i++)
+	{
+	    if(!(translationStringStream >> translationValues[i]))
+	    {
+			cerr << "An error occured while trying to parse the initial "
+				 << "translation." << endl
+				 << "No initial translation will be used" << endl;
+			return parsedTranslation;
+	    }
+	}
+	float extraOutput = 0;
+	if((translationStringStream >> extraOutput))
+    {
+		cerr << "Wrong initial translation size" << endl
+			 << "No initial translation will be used" << endl;
+		return parsedTranslation;
+	}
+	for(int i = 0; i < cloudDimension; i++)
+    {
+		parsedTranslation(i,cloudDimension) = translationValues[i];
+	}
+	return parsedTranslation;
+}
+
+PM::TransformationParameters parseRotation(string &rotation, const int cloudDimension)
+{
+	PM::TransformationParameters parsedRotation;
+	parsedRotation = PM::TransformationParameters::Identity(cloudDimension+1,cloudDimension+1);
+	rotation.erase(std::remove(rotation.begin(), rotation.end(), '['), rotation.end());
+	rotation.erase(std::remove(rotation.begin(), rotation.end(), ']'), rotation.end());
+	std::replace( rotation.begin(), rotation.end(), ',', ' ');
+	std::replace( rotation.begin(), rotation.end(), ';', ' ');
+	float rotationMatrix[9] = {0};
+	stringstream rotationStringStream(rotation);
+	for(int i = 0; i < cloudDimension*cloudDimension; i++)
+    {
+		if(!(rotationStringStream >> rotationMatrix[i]))
+        {
+			cerr << "An error occured while trying to parse the initial "
+				 << "rotation." << endl
+				 << "No initial rotation will be used" << endl;
+			return parsedRotation;
+		}
+	}
+	float extraOutput = 0;
+	if((rotationStringStream >> extraOutput))
+    {
+		cerr << "Wrong initial rotation size" << endl
+			 << "No initial rotation will be used" << endl;
+		return parsedRotation;
+	}
+	for(int i = 0; i < cloudDimension*cloudDimension; i++)
+    {
+		parsedRotation(i/cloudDimension,i%cloudDimension) = rotationMatrix[i];
+	}
+	return parsedRotation;
 }
 
 // main
@@ -52,6 +125,7 @@ int main(int argc, char **argv)
     while(ros::ok())
     {
         PM::ICP icp;
+        int cloudDimension = cloud2.getEuclideanDim();
     	PM::TransformationParameters translation = parseTranslation(initTranslation, cloudDimension);
     	PM::TransformationParameters rotation =	parseRotation(initRotation, cloudDimension);
     	PM::TransformationParameters initTransfo = translation*rotation;
