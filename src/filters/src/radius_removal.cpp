66ad67cbd45b39e6b1ea77534930d072d6583acd
Enes Demirağ <demirag16@itu.edu.tr>
2018-09-05 15:36:21 +0300

    statistical removal is slightly faster than radius removal

--- src/filters/src/radius_removal.cpp
+++ src/filters/src/radius_removal.cpp
@@ -19,29 +19,17 @@ std::string published_topic;
 double radius;
 int minNeighbors;
 
+pcl::PCLPointCloud2* passed_cloud = new pcl::PCLPointCloud2;
+pcl::PCLPointCloud2ConstPtr cloudPtr(passed_cloud);
+pcl::PCLPointCloud2 cleaned_cloud;
+
+sensor_msgs::PointCloud2 output_cloud;
+
 // callback
 void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-    //Create point cloud objects for passed_cloud and cleaned_cloud
-    pcl::PCLPointCloud2* passed_cloud = new pcl::PCLPointCloud2;
-    pcl::PCLPointCloud2ConstPtr cloudPtr(passed_cloud);
-    pcl::PCLPointCloud2 cleaned_cloud;
-
     // Convert to PCL data type
     pcl_conversions::toPCL(*cloud_msg, *passed_cloud);
-
-    // Perform the Radius Outlier Removal Filter
-    pcl::RadiusOutlierRemoval<pcl::PCLPointCloud2> r_filter;
-    r_filter.setInputCloud(cloudPtr); // Pass passed_cloud to the filter
-    r_filter.setRadiusSearch(radius); // Set radius
-    r_filter.setMinNeighborsInRadius(minNeighbors); // Set minimum neighbor number
-    r_filter.filter(cleaned_cloud); // Store output data in cleaned_cloud
-
-    // Convert to ROS data type
-    sensor_msgs::PointCloud2 output_cloud;
-    pcl_conversions::moveFromPCL(cleaned_cloud, output_cloud);
-
-    pub.publish(output_cloud); // Publish cleaned_cloud to the /cloud_cleaned topic
 }
 
 // main
@@ -50,13 +38,14 @@ int main(int argc, char **argv)
     // Initialize ROS
     ros::init(argc, argv, "radius_removal");
     ros::NodeHandle n;
+    ros::Rate r(10);
 
     // Load parameters from launch file
     ros::NodeHandle nh_private("~");
     nh_private.param<std::string>("subscribed_topic", subscribed_topic, "/cloud_passed");
     nh_private.param<std::string>("published_topic", published_topic, "cloud_cleaned");
     nh_private.param<double>("radius", radius, 0.5);
-    nh_private.param<int>("minNeighbors", minNeighbors, 10);
+    nh_private.param<int>("minNeighbors", minNeighbors, 100);
 
     // Create Subscriber and listen /cloud_passed topic
     ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>(subscribed_topic, 1, cloud_cb);
@@ -64,7 +53,19 @@ int main(int argc, char **argv)
     // Create Publisher
     pub = n.advertise<sensor_msgs::PointCloud2>(published_topic, 1);
 
-    // Spin
-    ros::spin();
+    while(ros::ok())
+    {
+        // Perform the Radius Outlier Removal Filter
+        pcl::RadiusOutlierRemoval<pcl::PCLPointCloud2> r_filter;
+        r_filter.setInputCloud(cloudPtr); // Pass passed_cloud to the filter
+        r_filter.setRadiusSearch(radius); // Set radius
+        r_filter.setMinNeighborsInRadius(minNeighbors); // Set minimum neighbor number
+        r_filter.filter(cleaned_cloud); // Store output data in cleaned_cloud
+        pcl_conversions::moveFromPCL(cleaned_cloud, output_cloud);
+        pub.publish(output_cloud); // Publish cleaned_cloud to the /cloud_cleaned topic
+
+        r.sleep();
+        ros::spinOnce();
+    }
     return 0;
 }
